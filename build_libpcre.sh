0f1467ea64714ba2a0e3288405636ba10cc9415f
Tom Petr <tom@r2c.dev>
2023-02-15 10:45:51 -0500

    try this

--- build_libpcre.sh
+++ build_libpcre.sh
@@ -3,11 +3,6 @@
 export PCRE_VERSION="8.45"
 export EM_CACHE="$(pwd)/.emscripten-cache"
 
-export DOWNLOAD_URL="https://sourceforge.net/projects/pcre/files/pcre/8.45/pcre-8.45.tar.gz/download"
-
-echo "Download: $DOWNLOAD_URL"
-curl -L $DOWNLOAD_URL | tar xvz
-
 pushd pcre-${PCRE_VERSION}
 
 emconfigure ./configure --enable-utf8
