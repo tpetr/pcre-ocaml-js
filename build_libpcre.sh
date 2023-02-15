daddec8fa16f21bb0491bf0cb62449d94f1242af
Tom Petr <tom@r2c.dev>
2023-02-15 10:55:01 -0500

    try this

--- build_libpcre.sh
+++ build_libpcre.sh
@@ -3,12 +3,15 @@
 export PCRE_VERSION="8.45"
 export EM_CACHE="$(pwd)/.emscripten-cache"
 
+cd ${1:-$(pwd)}
+
+echo "pwd: $(pwd)"
+
 pushd pcre-${PCRE_VERSION}
 
 emconfigure ./configure --enable-utf8
 
 patch Makefile ../makefile-wasm.patch -o Makefile.wasm
-
 V=1 emmake make -f Makefile.wasm libpcre.js
 
 popd
